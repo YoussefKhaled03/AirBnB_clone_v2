6f1a381e3f36bba7983e5999421d3ef15939f4aa
YoussefKhaled03 <ykte03@gmail.com>
2024-04-21 02:14:34 +0200

    adjust

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that starts a Flask web application
+A script that starts a Flask web application:
 """
 
 from flask import Flask
@@ -8,13 +8,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-	"""
-	Function that returns a string
-	"""
-	return "Hello HBNB!"
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    Displays 'Hello HBNB!'
+    """
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
