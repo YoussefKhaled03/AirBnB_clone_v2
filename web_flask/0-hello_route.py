22127e4af56a9be46b0c4703b2a7bdc6e92f6002
YoussefKhaled03 <ykte03@gmail.com>
2024-04-21 02:07:45 +0200

    Fix indentation in hello route function

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,11 +10,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-		"""
-		Function that returns a string
-		"""
-		return "Hello HBNB!"
+	"""
+	Function that returns a string
+	"""
+	return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-		app.run(host='0.0.0.0', port=5000)
+	app.run(host='0.0.0.0', port=5000)
